e992964572fd84cc9d1f53e5f39879ad69d48f37
matthewsorrell <sorrell.matthew@gmail.com>
2018-10-29 20:40:05 -0500

    c++ Hello world

---
+++ c++_hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char h = 'H', e = 'e', l = 'l', o = 'o';
+    char w = 'W', r = 'r', d = 'd';
+    
+    cout<<h + e + l + l + o + ' ' + w + o + r + l + d << endl;
+    cout << "Hello World";
+
+    return 0;
+}
