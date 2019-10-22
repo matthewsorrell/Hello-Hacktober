a2c48e95b35ea597520c84e0980c95bd7902a98b
matthewsorrell <sorrell.matthew@gmail.com>
2019-10-21 19:54:55 -0500

    Hello Hacktober - Java array

---
+++ hello-hacktober.java
@@ -0,0 +1,10 @@
+public class Hacktober {
+	public static void main(String [] args) {
+		String [] word = {"H", "e", "l", "l", "o", " ", "H", "a"
+		    ,"c", "k", "t", "o", "b", "e", "r", "!"
+		};
+		for(int i = 0; i < word.length; i++) {
+			System.out.print(word[i]);
+		}
+	}
+}
