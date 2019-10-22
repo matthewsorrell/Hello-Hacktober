2fb29f10f43259976132d0b2d90faba207cf5106
matthewsorrell <sorrell.matthew@gmail.com>
2019-10-21 20:25:55 -0500

    Hello world

---
+++ hello-hacktober.java
@@ -0,0 +1,9 @@
+public class Hacktober {
+	public static void main(String [] args) {
+		String [] word = {"H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", "!"
+		};
+		for(int i = 0; i < word.length; i++) {
+			System.out.print(word[i]);
+		}
+	}
+}
